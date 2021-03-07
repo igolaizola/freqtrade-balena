4c97db95ac1a5e3e3ed8a5e1c22733e0d397e4a2
Iñigo Garcia Olaizola <11333576+igolaizola@users.noreply.github.com>
2021-03-07 02:30:50 +0100

    Update startup candle count

--- strategies/SmaRsi.py
+++ strategies/SmaRsi.py
@@ -26,7 +26,7 @@ class SmaRsi(IStrategy):
     # Optimal timeframe for the strategy
     timeframe = '5m'
 
-    startup_candle_count = 1500
+    startup_candle_count = 1499
     
     stoploss = -0.15
     use_custom_stoploss = True
