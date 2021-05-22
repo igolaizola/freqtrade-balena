467f6153a45e5e050056d7e2855293512032960f
igolaizola <11333576+igolaizola@users.noreply.github.com>
2021-05-23 01:55:06 +0200

    Fix tghook

--- tghook/tghook.sh
+++ tghook/tghook.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-/bin/tghook -channel igotest01 -filter "COIN IS[: ]*([A-Z]+)" \
+/bin/tghook -channel "$TGHOOK_CHANNEL" -filter "COIN IS[: ]*([A-Z]+)" \
 -url http://freqtrade_2:8080/api/v1/forcebuy \
 -method POST -data '{"pair": "$1/BTC"}' \
 -header "Content-Type: application/json" \
 -auth-user user -auth-pass pass \
+-wait 100ms \
 -upper true --trim true
