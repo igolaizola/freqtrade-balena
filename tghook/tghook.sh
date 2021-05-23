07ba876682bc6c16661139a51a2a637d2a76876a
igolaizola <11333576+igolaizola@users.noreply.github.com>
2021-05-23 05:30:48 +0200

    Adjust tghook regex

--- tghook/tghook.sh
+++ tghook/tghook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/bin/tghook -channel "$TGHOOK_CHANNEL" -filter "COIN IS[: ]*([A-Z]+)" \
+/bin/tghook -channel "$TGHOOK_CHANNEL" -filter "COIN IS[: ]*([a-Z]+)" \
 -url http://freqtrade_2:8080/api/v1/forcebuy \
 -method POST -data '{"pair": "$1/BTC"}' \
 -header "Content-Type: application/json" \
